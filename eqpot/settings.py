f8d9a1bee85165556175d28fb69df670206acd3e
Natalie Jane Edson <nataliejaneedson@gmail.com>
2020-04-05 10:42:28 -0700

    adds some config for heroku postgres database

--- eqpot/settings.py
+++ eqpot/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django_heroku
+import dj_database_url
 
 
 
@@ -24,6 +26,7 @@ if os.path.exists(os.path.join(BASE_DIR, "eqpot", "settings_local.py")):
 else: 
     SECRET_KEY = os.getenv("SECRET_KEY")
     PRODUCTION = os.getenv("PRODUCTION")
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -58,6 +61,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -90,13 +94,8 @@ WSGI_APPLICATION = 'eqpot.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
+DATABASES = {}
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
@@ -136,8 +135,14 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
+
+django_heroku.settings(locals())
+
+#del DATABASES['default']['OPTIONS']['sslmode']
